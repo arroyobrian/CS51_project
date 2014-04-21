b1a6788c146b1ff5b574e2ca5d330660f5685073
Brian Arroyo <brianarroyo@college.harvard.edu>
2014-04-20 22:33:36 -0400

    Minor function argument added; util file added
    Add a file, added arguments to functions

--- project/connectfour.py
+++ project/connectfour.py
@@ -1,10 +1,12 @@
 
 class NonexistentMove(Exception):
     """ if player makes an illegal request """
+    """ we will need a way to store the offending
+        column (and possibly the board itself) """
 
 
     
-class ConnectFourBoard(object):
+class ConnectFourBoard(board_matrix, current_player):
     """ Store a Connect-Four Board
 
     Board columns fill from the bottom (ie., row 6).
@@ -19,6 +21,7 @@ class ConnectFourBoard(object):
     def __init__():
         """ new ConnectFourBoard
         """
+
    
     def get_current_player_id():
         """ id of the player whose turn it is """
@@ -26,27 +29,31 @@ class ConnectFourBoard(object):
     def get_other_player_id():
         """ id of the opponent of the player who should be moving now """
    
-    def top_elt_column():
+    def top_elt_column(column):
         """
         id# of player who is in the topmost token in the column.
         Return 0 if the column is empty.
-        """
+        So a way to link a column with a player is needed. maybe
+        if we create a column id for each column and store the information
+        as an array?"""
 
-    def height_column():
+    def height_column(column):
         """
         Return the index of the first cell in the specified column that is filled.
         """
 
-    def cell():
+    def cell(column, row):
         """
         Get the id# of the player owning the taker in the specified cell.
         Return 0 if it is unclaimed.
         """
     
-    def move():
+    def move(column):
         """
         Make the move as the player.
         Raise 'InvalidMoveException' if move is illegal.
+        Takes in the column the player wants to change and 
+        their id to store their move
         """
         
     def win():
---
+++ project/util.py
@@ -0,0 +1,13 @@
+import numpy
+
+""" here we want to define helper functions. we may want to write functions that
+	may run checks or other useful functions """
+
+	def run_test_functions ():
+
+
+
+
+""" we may predefine winning boards for testing here to see how the computer runs in each case"""
+
+""" we would want to implement listeners/events here """
